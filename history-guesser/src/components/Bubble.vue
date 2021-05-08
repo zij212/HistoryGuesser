305998c79d21662f89baf954a20950ef34f77b30
Conor Patrick <conorpp94@gmail.com>
2021-05-08 13:16:52 -0700

    more stuff

---
+++ history-guesser/src/components/Bubble.vue
@@ -0,0 +1,44 @@
+<template>
+  <div >
+    <div class='quote'>
+    <img class='round-avatar' src='{{url}}'>
+    <div class='speech-bubble left'>
+        <p>
+        {{name}}
+        <span class='time-ago'>
+            {{time}}
+        </span>
+        </p>
+        <blockquote>
+            {{message}}
+        </blockquote>
+    </div>
+    </div>
+  </div>
+
+</template>
+
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    message: String,
+    name: String,
+    time: Date,
+    url: String,
+  }
+})
+export default class Bubble extends Vue {
+  message!: string
+  name!: string
+  time!: Date
+  url!:string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
+
